ccc63aa24b5dd69bc1b3168b7c430802e6c7ae90
felixa01226058 <fox43@live.com.mx>
2017-05-03 07:19:25 -0500

    score

--- routes/js/game.js
+++ routes/js/game.js
@@ -3,11 +3,6 @@
 var deck=[];
 var shuffleArray=[];
 
-//deck for each player on each turn
-var onHand = [];
-
-//deck generated during the round final deck combination each round
-var onPlay = [];
 
 //array with the players objects
 var playersArray = [];
@@ -20,18 +15,29 @@ function card(nombre,src,tipo,valor){
 	this.src = src;
 	this.type = tipo;
 	this.value = valor;
-}
+} 
 //PLAYER
 function player(nombre){
 
 	this.name = nombre;
 	this.onPlay = [];
 	this.onHand = [];
+	this.score = 0;
+	this.combos = {
+		tempuras: 0,
+		makis:0,
+		sashimis:0,
+		kiosas: 0,
+		puddings: 0
+		//wasabi:null
+	};
 }
 
 
 
+
 function startGame(players){
+
 	initDeck();
 
 	switch(players){
@@ -112,32 +118,202 @@ function startGame(players){
 			playersArray.push(player4);
 			playersArray.push(player5);
 
-
-
 			break;
 		default:
 
 	}
 
 	
-	console.log("players "+ playersArray.length);
+	console.log("number of players "+ playersArray.length);
 
 
 	console.log("player1 first card1 "+playersArray[0].onHand[0].name);
 	console.log("player1 first card2 "+playersArray[1].onHand[0].name);
-	console.log("player1 first card2 "+playersArray[2].onHand[0].name);
+	/*console.log("player1 first card2 "+playersArray[2].onHand[0].name);
 	console.log("player1 first card2 "+playersArray[3].onHand[0].name);
 	console.log("player1 first card2 "+playersArray[4].onHand[0].name);
+	console.log("combos "+playersArray[4].combos.tempura);*/
 
+	//swaponHand(playersArray,2);
+
+	console.log(playersArray[0].onHand.length);
+
+	//while(playersArray[0].onHand!=null){
+	console.log(playersArray[0].onHand);
+
+}
+
+
+function chooseCard(cardIndex){
+
+	if(playersArray[0].onHand.length != 0){
+
+		var takecard = playersArray[0].onHand[cardIndex];
+
+		playersArray[0].onHand.remove(cardIndex);
+
+		playersArray[0].onPlay.push(takecard);
+
+
+
+	}else{
+		console.log("No More Cards");
+	}
+
+
+	/*for (var i = 0; i < playersArray[0].onPlay.length; i++) {
+		console.log("onPlay"+playersArray[0].onPlay[i].name);
+	}
 	
 
+	for (var i = 0; i < playersArray[0].onHand.length; i++) {
+		console.log(playersArray[0].onHand[i].name);
+	}*/
 
+	console.log(playersArray[0].onHand.length);
+	console.log(playersArray[0].onHand);
+	console.log(playersArray[0].onPlay.length);
+	console.log(playersArray[0].onPlay);
+	
+	return playersArray[0].onHand;
+	
+}
+
+function getScore(){
+
+	for (var i = 0; i < playersArray.length; i++) {
+
+		for (var y = 0; y < playersArray[i].onPlay.length; y++) {
+
+			switch(playersArray[i].onPlay[y].type){
+
+				case "tempura" :
+					playersArray[i].combos.tempuras++;
+					break;
+				case "maki_roll" :
+					playersArray[i].combos.makis++;
+					break;
+				case "sashimi" :
+					playersArray[i].combos.sashimis++;
+					break;
+				case "dumpling" :
+					playersArray[i].combos.kiosas++;
+					break;
+				case "dumpling" :
+					playersArray[i].combos.kiosas++;
+					break;
+				case "pudding" :
+					playersArray[i].combos.puddings++;
+					break;
+				case "wasabi":
+					if((y < playersArray[i].onPlay.length-1)){
+						console.log("si entro");
+						if(playersArray[i].onPlay[y+1].type == "nigiri_salmon" || playersArray[i].onPlay[y+1].type == "nigiri_egg" || playersArray[i].onPlay[y+1].type == "nigiri_squid" ){
+
+							playersArray[i].score+=(playersArray[i].onPlay[y+1].value*2);
+						}
+					}
+
+
+
+
+
+
+
+					break;
+
+				default: 
+					break;
+
+			}
+
+
+			
+		}
+	}
+
+	console.log("los puddings"+playersArray[0].combos.puddings);
+	console.log("dumplings"+playersArray[0].combos.kiosas);
+	console.log("sashimis"+playersArray[0].combos.sashimis);
+	console.log("makis"+playersArray[0].combos.makis);
+	console.log("tempuras"+playersArray[0].combos.tempuras);
+
+	console.log("los puddings"+playersArray[1].combos.puddings);
+	console.log("dumplings"+playersArray[1].combos.kiosas);
+	console.log("sashimis"+playersArray[1].combos.sashimis);
+	console.log("makis"+playersArray[1].combos.makis);
+	console.log("tempuras"+playersArray[1].combos.tempuras);
+
+
+	for (var i = 0; i < playersArray.length; i++) {
+		
+		if(playersArray[i].combos.tempuras >=2 ){
+
+			var x = playersArray[i].combos.tempuras/2;
+
+			var answer = Math.floor(x);
+			console.log(answer);
+
+			playersArray[i].score+=answer*5;
+		}
+		if(playersArray[i].combos.sashimis >= 3){
+
+			var x = playersArray[i].combos.sashimis/3;
+
+			var answer = Math.floor(x);
+			console.log(answer);
+
+			playersArray[i].score+=answer*10;
+
+		}
+		if(playersArray[i].combos.makis >= 2 ){
+			
+			playersArray[i].score+=6;
+			
+		}
+		if(playersArray[i].combos.kiosas !=0 ){
+			if(playersArray[i].combos.kiosas == 1){
+				playersArray[i].score+=1;
+			}
+			if(playersArray[i].combos.kiosas ==2){
+				playersArray[i].score+=3;
+			}
+			if(playersArray[i].combos.kiosas == 3){
+				playersArray[i].score+=6;
+			}
+			if(playersArray[i].combos.kiosas == 4){
+				playersArray[i].score+=10;
+			}
+			if(playersArray[i].combos.kiosas >= 45){
+				playersArray[i].score+=15;
+			}else{
+
+			}
+			
+		}
+
+		if(playersArray[i].combos.puddings >= 3 ){
+			
+			playersArray[i].score+=10;
+			
+		}
+
+
+
+	}
+
+	console.log("score player1 "+playersArray[0].score);
+	console.log("score player2 "+playersArray[1].score);
 }
 
 
-function swaponHand(playersArray){
 
-}
+
+Array.prototype.remove = function(from, to) {
+  var rest = this.slice((to || from) + 1 || this.length);
+  this.length = from < 0 ? this.length + from : from;
+  return this.push.apply(this, rest);
+};
 
 
 function getHands(players){
@@ -154,6 +330,8 @@ function onPlay(){
 
 //START RANDOM DECK FOR THE GAME--------------------------------------------
 function initDeck(){
+	shuffleArray = [];
+	playersArray = [];
 	shuffleArray = shuffle(deck);
 	for (var i = 0; i < 9; i++) {
 		console.log(shuffleArray[i].name);
@@ -219,6 +397,8 @@ generateDeck().then(
 	deck = newdeck;
 	console.log(deck.length);
 });
+
+
 //----------------------------------------------------------------------------
 
 
