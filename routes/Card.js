b10cfe4486ba5364c15745b1352ed68c51a27246
Felix Iñiguez Iñiguez <fox43@live.com.mx>
2017-05-03 00:04:06 -0500

    final card

--- routes/Card.js
+++ routes/Card.js
@@ -1,1 +1,41 @@
+class Card {
+	constructor(nombre,src,tipo,valor){
+			this.name = nombre;
+			this.src = src;
+			this.type = tipo;
+			this.value = valor;
+	}
 
+	get cardname(){
+		return this.name;
+	}
+
+	set cardname(nombre){
+		this.name = nombre;
+	}
+
+	get src(){
+		return this.src;
+	}
+
+	set src(src){
+		this.src = src;
+	}
+  
+  get type(){
+		return this.type;
+	}
+
+	set type(tipo){
+		this.type = tipo;
+	}
+  
+  get value(){
+		return this.value;
+	}
+
+	set value(valor){
+		this.value = valor;
+	}
+  
+}
